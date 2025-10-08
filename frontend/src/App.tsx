7546a9a354e98e0dea22d3e7ba34c2d603a88727
lizzymak <146508325+lizzymak@users.noreply.github.com>
2025-10-08 00:31:33 -0700

    began frontend and added news api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>News setiment analyzer</h1>
+      <input value={query} onChange={e => setQuery(e.target.value)} placeholder="Enter company or topic..." />
+      <button>Analyze</button>
     </div>
   );
 }
